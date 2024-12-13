631bbc35360bf558f2bc91e19982982fa6c095c4
Phil Shapiro <philshap@gmail.com>
2024-12-13 11:47:23 -0500

    day 13 test data

--- src/advent2024/Day13.java
+++ src/advent2024/Day13.java
@@ -65,8 +65,28 @@ public class Day13 extends Day {
   }
 
   public static void main(String[] args) {
-    var day = new Day13();
-    System.out.println(day.part1());
-    System.out.println(day.part2());
+    var day = new Day13() {
+      @Override
+      String getData() {
+        return """
+            Button A: X+94, Y+34
+            Button B: X+22, Y+67
+            Prize: X=8400, Y=5400
+
+            Button A: X+26, Y+66
+            Button B: X+67, Y+21
+            Prize: X=12748, Y=12176
+
+            Button A: X+17, Y+86
+            Button B: X+84, Y+37
+            Prize: X=7870, Y=6450
+
+            Button A: X+69, Y+23
+            Button B: X+27, Y+71
+            Prize: X=18641, Y=10279""";
+      }
+    };
+    System.out.println(day.part1().equals("480"));
+    System.out.println(day.part2().equals("875318608908"));
   }
 }
